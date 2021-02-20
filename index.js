7c3fa335e56e4f2d762578b7d856d6eef10f38b9
edehlol <edymusajev@gmail.com>
2021-02-20 23:09:03 +0200

    added MongoDB + mongoose. Finished the OAuth authentication flow. Added cookie functionality for express

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const { mongoURI, cookieKey } = require('./config/keys');
+
+// First load in the mongoose model
+require('./models/User');
+// Then load in the passport code
 require('./services/passport');
 
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,22 @@ const passport = require('passport');
 // route handlers
 module.exports = (app) => {
   app.get(
+    //   PATH -> path on the server (url)
     '/auth/google',
+    // HANDLER -> function executed when the route is matched
     passport.authenticate('google', {
       // what info do I want to request from a user's account
       scope: ['profile', 'email'],
     })
   );
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
